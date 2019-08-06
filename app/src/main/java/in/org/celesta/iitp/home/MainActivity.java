4c3d4429206792b1d77ad0608c06c8eceb1a84e8
Aman Jee <amangrobo@gmail.com>
2019-08-06 08:13:20 +0530

    added nav items and icons

--- app/src/main/java/in/org/celesta/iitp/home/MainActivity.java
+++ app/src/main/java/in/org/celesta/iitp/home/MainActivity.java
@@ -77,16 +77,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
