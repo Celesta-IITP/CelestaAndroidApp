d7dbcc191f77c712bfad9dea2269aa592f5fbef1
somenath sarkar <somenath1435@gmail.com>
2019-08-29 00:51:13 +0530

    added textwatcher

--- app/src/main/java/in/org/celesta/iitp/Auth/LoginFragment.java
+++ app/src/main/java/in/org/celesta/iitp/Auth/LoginFragment.java
@@ -7,15 +7,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import in.org.celesta.iitp.R;
 
 public class LoginFragment extends Fragment {
     TextView register_textview;
+    EditText login_celesta_id_editext,login_password_edittext;
+    Button login_button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +29,9 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_login, container, false);
         register_textview=rootView.findViewById(R.id.register_textview);
+        login_celesta_id_editext=rootView.findViewById(R.id.login_celesta_Id_edittext);
+        login_password_edittext=rootView.findViewById(R.id.login_password_edittext);
+        login_button=rootView.findViewById(R.id.login_button);
         return rootView;
     }
 
@@ -35,6 +44,9 @@ public class LoginFragment extends Fragment {
                 loadFragment(new RegisterFragment());
             }
         });
+
+        login_celesta_id_editext.addTextChangedListener(loginTextWatcher);
+        login_password_edittext.addTextChangedListener(loginTextWatcher);
     }
 
     private boolean loadFragment(Fragment fragment) {
@@ -48,4 +60,24 @@ public class LoginFragment extends Fragment {
         }
         return false;
     }
+
+    private TextWatcher loginTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String celestaIdInput = login_celesta_id_editext.getText().toString().trim();
+            String passwordInput = login_password_edittext.getText().toString().trim();
+
+            login_button.setEnabled(!celestaIdInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
