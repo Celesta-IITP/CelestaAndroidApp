2468b9f11a1e24c8a5b82df937af87e532bf02d1
Aman Jee <amangrobo@gmail.com>
2019-08-28 18:28:58 +0530

    added gallery

---
+++ app/src/main/java/in/org/celesta/iitp/gallery/GalleryFragment.java
@@ -0,0 +1,25 @@
+package in.org.celesta.iitp.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import in.org.celesta.iitp.R;
+
+public class GalleryFragment extends Fragment {
+
+    public GalleryFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        return view;
+    }
+
+}
