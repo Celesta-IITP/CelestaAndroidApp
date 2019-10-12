e4c2e606c827c165dec722034fee63492181cc23
somenath sarkar <somenath1435@gmail.com>
2019-10-13 04:55:22 +0530

    Added offline mode for profile

--- app/src/main/java/in/org/celesta/iitp/Auth/ProfileFragment.java
+++ app/src/main/java/in/org/celesta/iitp/Auth/ProfileFragment.java
@@ -4,6 +4,7 @@ package in.org.celesta.iitp.Auth;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,11 +17,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import in.org.celesta.iitp.R;
 import in.org.celesta.iitp.home.MainActivity;
 import in.org.celesta.iitp.network.RetrofitClientInstance;
+import in.org.celesta.iitp.utils.CheckNetwork;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
@@ -29,6 +36,8 @@ import retrofit2.Response;
 
 public class ProfileFragment extends Fragment {
     private Button logout_button;
+    private TextView celestaid_textview, first_name_textview;
+    private ImageView qrcode_imageview;
     private SharedPreferences.Editor sharedPreferenceEditor;
     private SharedPreferences preferences;
     private AuthApi authApi;
@@ -39,46 +48,56 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        preferences=PreferenceManager.getDefaultSharedPreferences(getContext());
-        logout_button=rootView.findViewById(R.id.profile_logout_button);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        logout_button = rootView.findViewById(R.id.profile_logout_button);
+        celestaid_textview = rootView.findViewById(R.id.profile_celestaid);
+        first_name_textview = rootView.findViewById(R.id.profile_first_name);
+        qrcode_imageview = rootView.findViewById(R.id.profile_qrcode);
         return rootView;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         logout_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                logout();
+                if (!CheckNetwork.isNetworkConnected(getContext()))
+                    Toast.makeText(getContext(), "Check Internet connection", Toast.LENGTH_SHORT).show();
+                else
+                    logout();
             }
         });
+
+        if (!CheckNetwork.isNetworkConnected(getContext()))
+            showOfflineProfile();
     }
 
-    private void logout(){
+    private void logout() {
         logoutApi();
         clearSharedPreference();
     }
 
-    private void clearSharedPreference(){
+    private void clearSharedPreference() {
         sharedPreferenceEditor = preferences.edit();
-        sharedPreferenceEditor.putBoolean("login_status",false);
-        sharedPreferenceEditor.putString("celestaid","");
-        sharedPreferenceEditor.putString("access_token","");
-        sharedPreferenceEditor.putString("first_name","");
-        sharedPreferenceEditor.putString("qrcode","");
+        sharedPreferenceEditor.putBoolean("login_status", false);
+        sharedPreferenceEditor.putString("celestaid", "");
+        sharedPreferenceEditor.putString("access_token", "");
+        sharedPreferenceEditor.putString("first_name", "");
+        sharedPreferenceEditor.putString("qrcode", "");
         sharedPreferenceEditor.apply();
     }
 
-    private void logoutApi(){
-        String celestaid=preferences.getString("celestaid","");
-        String access_token=preferences.getString("access_token","");
+    private void logoutApi() {
+        String celestaid = preferences.getString("celestaid", "");
+        String access_token = preferences.getString("access_token", "");
 
         progressDialog = new ProgressDialog(getContext());
         progressDialog.setMessage("Logging out...");
         progressDialog.show();
 
-        authApi= RetrofitClientInstance.getAuthRetrofitInstance().create(AuthApi.class);
+        authApi = RetrofitClientInstance.getAuthRetrofitInstance().create(AuthApi.class);
 
         RequestBody requestBody = new MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
@@ -93,24 +112,37 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onResponse(Call<LogoutResponse> call, Response<LogoutResponse> response) {
                 progressDialog.dismiss();
-                if(response.body().getStatus()==202){
-                    Toast.makeText(getContext(),response.body().getMessage().get(0),Toast.LENGTH_SHORT).show();
-                    Intent intent=new Intent(getContext(), MainActivity.class);
+                if (response.body().getStatus() == 202) {
+                    Toast.makeText(getContext(), response.body().getMessage().get(0), Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getContext(), MainActivity.class);
                     startActivity(intent);
                     getActivity().finish();
-                }
-                else{
-                    Toast.makeText(getContext(),response.body().getMessage().get(0),Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(), response.body().getMessage().get(0), Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<LogoutResponse> call, Throwable t) {
                 progressDialog.dismiss();
-                Log.e("failed", "onFailure: "+t.toString() );
-                Toast.makeText(getContext(),"Something went wrong",Toast.LENGTH_SHORT).show();
+                Log.e("failed", "onFailure: " + t.toString());
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void showOfflineProfile() {
+        String qrcode = preferences.getString("qrcode", "");
+        Glide.with(getContext())
+                .load(qrcode)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(qrcode_imageview);
+
+        String celestaid = "Celesta ID: " + preferences.getString("celestaid", "");
+        celestaid_textview.setText(celestaid);
+
+        String first_name = "Hello " + preferences.getString("first_name", "");
+        first_name_textview.setText(first_name);
     }
 
 }
